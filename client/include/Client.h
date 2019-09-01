e1b35ee80ff6226011330a8adafdc40aea322dcb
mariav <mariaviseu@gmail.com>
2019-09-01 18:32:41 +0100

    Add Client destructor
    * closes the socket
    * calls get on read/write futures to ensure write/read threads finish and exceptions are propagated

--- client/include/Client.h
+++ client/include/Client.h
@@ -1,17 +1,24 @@
 #pragma once
 #include "HostPort.h"
 #include <boost/asio.hpp>
+#include <future>
 
 namespace client {
 
 class Client {
 public:
   Client() : socket_(ioContext_) {}
+  ~Client();
   auto Run(const HostPort &hostport) -> void;
 
 private:
+  auto Read() -> void;
+  auto Write() -> void;
   boost::asio::io_context ioContext_;
   boost::asio::ip::tcp::socket socket_;
+  std::mutex mtxSocket_;
+  std::future<void> readFut_;
+  std::future<void> writeFut_;
 };
 
 } // namespace client
--- client/src/Client.cpp
+++ client/src/Client.cpp
@@ -1,11 +1,27 @@
 #include "Client.h"
+#include <iostream>
 
 namespace client {
+
+auto Client::Read() -> void {}
+
+auto Client::Write() -> void {}
 
 auto Client::Run(const HostPort &hostport) -> void {
   boost::asio::ip::tcp::resolver resolver(ioContext_);
   auto endpoints = resolver.resolve(hostport.host, hostport.port);
   boost::asio::connect(socket_, endpoints);
+  readFut_ = std::async(&Client::Read, this);
+  writeFut_ = std::async(&Client::Write, this);
+}
+
+Client::~Client() {
+  std::unique_lock<std::mutex> lck(mtxSocket_);
+  socket_.shutdown(boost::asio::ip::tcp::socket::shutdown_both);
+  socket_.close();
+  lck.unlock();
+  readFut_.get();
+  writeFut_.get();
 }
 
 } // namespace client
